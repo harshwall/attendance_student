18e6afe0928f53769d55f464a386640db157690e
Saket Kumar <sk846694@gmail.com>
2019-08-31 22:17:51 +0530

    Login Screen data receiving.

---
+++ lib/classes/student.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class Student {
+	String _regNo;
+	String _pass;
+	String _name;
+	String _father;
+	String _gender;
+	String _category;
+	DateTime _dob;
+	String _email;
+	String _mobile;
+	String _documentId;
+
+	Student.loginPage(this._regNo, this._pass);
+
+
+	Student.blank();
+
+	Student(this._regNo, this._pass, this._name, this._father, this._gender,
+		this._category, this._dob, this._email, this._mobile, this._documentId);
+
+	String get mobile => _mobile;
+
+	set mobile(String value) {
+		_mobile = value;
+	}
+
+
+	String get documentId => _documentId;
+
+	set documentId(String value) {
+		_documentId = value;
+	}
+
+	String get email => _email;
+
+	set email(String value) {
+		_email = value;
+	}
+
+	DateTime get dob => _dob;
+
+	set dob(DateTime value) {
+		_dob = value;
+	}
+
+	String get category => _category;
+
+	set category(String value) {
+		_category = value;
+	}
+
+	String get gender => _gender;
+
+	set gender(String value) {
+		_gender = value;
+	}
+
+	String get father => _father;
+
+	set father(String value) {
+		_father = value;
+	}
+
+	String get name => _name;
+
+	set name(String value) {
+		_name = value;
+	}
+
+	String get pass => _pass;
+
+	set pass(String value) {
+		_pass = value;
+	}
+
+	String get regNo => _regNo;
+
+	set regNo(String value) {
+		_regNo = value;
+	}
+
+	Map<String, dynamic> toMap() {
+		Map<String, dynamic> map;
+		map['regNo'] =_regNo;
+		map['pass'] = _pass;
+		map['name'] = _name;
+		map['father'] = _father;
+		map['gender'] = _gender;
+		map['category'] = _category;
+		map['dob'] = _dob;
+		map['email'] = _email;
+		map['mobile'] = _mobile;
+
+		return map;
+	}
+
+	Student.fromMapObject(Map<String, dynamic> map) {
+		this._regNo = map['regNo'];
+		this._pass = map['pass'];
+		this._name = map['name'];
+		this._father = map['father'];
+		this._gender = map['gender'];
+		this._category = map['category'];
+		this._dob = map['dob'];
+		this._email = map['email'];
+		this._mobile = map['mobile'];
+		this._documentId = map[''];
+	}
+
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,9 @@
+import 'package:attendance_student/classes/student.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:password/password.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -10,8 +15,14 @@ class Login extends StatefulWidget {
 class LoginState extends State<Login> {
   var _loginForm = GlobalKey<FormState>();
 
+  Student student = Student.blank();
+  String inputPass="";
+
   @override
   Widget build(BuildContext context) {
+
+    Student incoming = Student.blank();
+
     return Scaffold(
         appBar: AppBar(
           title: Text('Login'),
@@ -26,6 +37,9 @@ class LoginState extends State<Login> {
                   Padding(
                     padding: EdgeInsets.all(10.0),
                     child: TextFormField(
+                      onSaved: (value) {
+                        student.regNo = value;
+                      },
                       validator: (String value) {
                         if (value.length != 8)
                           return 'Incorrect Registration Number';
@@ -41,8 +55,12 @@ class LoginState extends State<Login> {
                     padding: EdgeInsets.all(10.0),
                     child: TextFormField(
                       obscureText: true,
+                      onSaved: (value) {
+                        inputPass = value;
+                      },
                       validator: (String value) {
-                        if (value.length < 6) return 'Password too short';
+                        if (value.length < 6)
+                          return 'Password too short';
                       },
                       decoration: InputDecoration(
                           labelText: 'Password',
@@ -57,26 +75,65 @@ class LoginState extends State<Login> {
                         child: Text('Login'),
                         elevation: 20.0,
                         onPressed: () {
-                          setState(() {
-                            if (_loginForm.currentState.validate()) {
-                              _loginForm.currentState.save();
-                            }
+
+                          bool check=false;
+                          if (_loginForm.currentState.validate()) {
+                            _loginForm.currentState.save();
+
+                            Fluttertoast.showToast(
+                                msg: student.regNo,
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIos: 1,
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white,
+                                fontSize: 16.0
+                            );
+//                            check = checkLogin(incoming);
+//                            if(check==true)
+//                              student=incoming;
+
+
+                          Firestore.instance.collection('stud')
+                            .where('regNo', isEqualTo: student.regNo)
+                            .where('pass', isEqualTo: inputPass)
+                            .getDocuments()
+                            .then((QuerySnapshot docs) {
+                              try {
+                                incoming = Student.fromMapObject(
+                                    docs.documents[0].data);
+                                check=true;
+                              }
+                              catch(e){
+                                check=false;
+                              }
+                             setState(() {
+
+                             });
+                             Fluttertoast.showToast(
+                                 msg: check.toString(),
+                                 toastLength: Toast.LENGTH_SHORT,
+                                 gravity: ToastGravity.CENTER,
+                                 timeInSecForIos: 1,
+                                 backgroundColor: Colors.red,
+                                 textColor: Colors.white,
+                                 fontSize: 16.0
+                             );
                           });
+                          }
+
+
                         }),
                   ),
                   Padding(
                     padding: EdgeInsets.all(10.0),
                     child: RaisedButton(
                       child: Text(
-                          'New User? Sign Up',
-                        style: TextStyle(
-                          color: Colors.black
-                        ),
+                        'New User? Sign Up',
+                        style: TextStyle(color: Colors.black),
                       ),
                       color: Colors.white,
-                      onPressed: (){
-
-                      },
+                      onPressed: () {},
                     ),
                   )
                 ],
@@ -84,5 +141,22 @@ class LoginState extends State<Login> {
             ),
           ),
         ));
+  }
+
+  bool checkLogin(Student incoming) {
+    final algorithm = PBKDF2();
+//    final hash = Password.hash(inputPass, algorithm);
+    final hash=inputPass;
+     Firestore.instance.collection('stud')
+    .where('regNo', isEqualTo: student.regNo)
+    .where('pass', isEqualTo: hash).getDocuments()
+    .then((QuerySnapshot docs) async {
+      if(docs.documents.length!=0)
+      incoming = Student.fromMapObject(docs.documents[0].data);
+    });
+    if(incoming == null)
+        return false;
+    else
+        return true;
   }
 }
